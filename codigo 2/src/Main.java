515d4afe3bfc909161ee09a611ce4ec9eecd0d73
Leandro <leandro.jacho2003@gmail.com>
2026-04-14 21:56:39 -0500

    Prueba de todo el código

---
+++ codigo 2/src/Main.java
@@ -0,0 +1,33 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
+public static void main() {
+
+    Scanner sc = new Scanner(System.in);
+
+    Paciente p1 = new Paciente();
+
+    System.out.println("Ingrese su nombre: ");
+    p1.nombre = sc.nextLine();
+
+    System.out.println("Ingrese su cedula: ");
+    p1.cedula = sc.nextInt();
+
+    System.out.println("Ingrese su tipo de seguro(basico/premium/sin seguro): ");
+    p1.setTiposeguro(sc.nextLine());
+
+    System.out.println("Ingrese su edad: ");
+    p1.setEdad((sc.nextInt()));
+
+    System.out.println("Ingrese el costo de la consulta: ");
+    p1.setCosto(sc.nextDouble());
+
+    System.out.println("Cuantas consultas tuvo: ");
+    p1.setConsultasRealizadas(sc.nextInt());
+
+    System.out.println("Que prioridad es(1 al 5): ");
+    p1.setPrioridad(sc.nextInt());
+
+}
---
+++ codigo 2/src/Paciente.java
@@ -0,0 +1,86 @@
+public class Paciente {
+
+    public String nombre;
+    public int cedula;
+    private String tiposeguro;
+    public int edad;
+    private double costo;
+    private int consultasRealizadas;
+    private int prioridad;
+
+    public void setTiposeguro(String tiposeguro) {
+        this.tiposeguro = tiposeguro;
+    }
+
+
+    public void setEdad(int edad){
+        if (edad>0){
+            this.edad=edad;
+
+        }else{
+            System.out.println("Edad no valida");
+        }
+    }
+
+    public void setCosto(double costo){
+        if (costo >0){
+            this.costo = costo;
+        }else{
+            System.out.println("Costo no valido");
+        }
+    }
+
+    public void setConsultasRealizadas(int consultasRealizadas){
+        if (consultasRealizadas>0){
+            this.consultasRealizadas = consultasRealizadas;
+        }else{
+            System.out.println("Numero no valido");
+        }
+    }
+
+    public void setPrioridad(int prioridad){
+        if (prioridad >= 1 && prioridad<=5){
+            this.prioridad = prioridad;
+        }else{
+            System.out.println("Prioridad no valida");
+        }
+    }
+
+    public double costoparcial() {
+        return costo * consultasRealizadas;
+    }
+
+    public double descuento() {
+        if (tiposeguro.equals("basico")) {
+            return costoparcial() - (costoparcial() * 0.10);
+        } else if (tiposeguro.equals("premium")) {
+            return costoparcial() - (costoparcial() * 0.20);
+        } else if (tiposeguro.equals("sin seguro")) ;
+        return costoparcial();
+    }
+
+    public String clasificar() {
+        if (prioridad == 4 || prioridad == 5) {
+            return "Alta prioridad";
+        } else if (prioridad == 2 || prioridad == 3) {
+            return "Prioridad media";
+        } else if (prioridad == 1) {
+            return "Prioridad baja";
+        }
+        return "";
+    }
+
+    public String mensaje() {
+        if (prioridad == 4 || prioridad == 5) {
+            return "Atención inmediata";
+        } else if (prioridad == 2 || prioridad == 3) {
+            return "Seguimiento necesario";
+        } else if (prioridad == 1) {
+            return "Control regular";
+        }
+        return "";
+    }
+
+
+
+}
